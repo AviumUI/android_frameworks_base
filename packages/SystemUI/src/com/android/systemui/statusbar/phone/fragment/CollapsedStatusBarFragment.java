d9a8fe2606193780f09be43ed8e364b8cb8208c7
AmeChanRain <downloadbot007@gmail.com>
2025-03-31 17:23:38 +0800

    statusbar: BP1A fixup for status bar lyric
    Change-Id: I34f5d95bc6d2f7ff9a0f6f327d304d43587bd398

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/fragment/CollapsedStatusBarFragment.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/fragment/CollapsedStatusBarFragment.java
@@ -408,7 +408,7 @@ public class CollapsedStatusBarFragment extends Fragment implements CommandQueue
         initOperatorName();
         initNotificationIconArea();
         mLyricController = new LyricController(getContext(), mStatusBar);
-        mStatusBarFragmentComponent.getHeadsUpAppearanceController().setLyricViewController(mLyricController);
+        mHomeStatusBarComponent.getHeadsUpAppearanceController().setLyricViewController(mLyricController);
         Dependency.get(TunerService.class).addTunable(this, Settings.Secure.STATUS_BAR_SHOW_LYRIC);
         mSystemEventAnimator =
                 new StatusBarSystemEventAnimator(mEndSideContent, getResources());

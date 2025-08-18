9a027c58e581671e36e31595bde90482cf3c225e
ezio84 <brabus84@gmail.com>
2025-08-18 20:11:33 +0200

    derpUtils: Reverse actions for QS and notification panel
    This just makes sense, so lets do it!
    
    - Update method names in PWM
    
    Change-Id: Ic08bee12c90dc55265dea3c3972f4087b93d2826
    Signed-off-by: DennySPb <dennyspb@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/CentralSurfaces.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/CentralSurfaces.java
@@ -200,7 +200,9 @@ public interface CentralSurfaces extends Dumpable, LifecycleOwner, CoreStartable
 
     void onKeyguardViewManagerStatesUpdated();
 
-    /**  */
+    void toggleSettingsPanel();
+
+    /** */
     boolean getCommandQueuePanelsEnabled();
 
     void showWirelessChargingAnimation(int batteryLevel);

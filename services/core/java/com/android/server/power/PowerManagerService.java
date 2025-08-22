5dee62e6a6fef5d8649c8a31cbe3c74e42d8b6af
ruyue <ruyueee@outlook.com>
2025-08-22 18:02:02 +0800

    fixup: add HandlerThread to send broadcast
    Change-Id: Ie821311c4f29f48cf48d21f62ad9af9607660913

--- services/core/java/com/android/server/power/PowerManagerService.java
+++ services/core/java/com/android/server/power/PowerManagerService.java
@@ -154,6 +154,8 @@ import com.android.server.power.batterysaver.BatterySaverPolicy;
 import com.android.server.power.batterysaver.BatterySaverStateMachine;
 import com.android.server.power.batterysaver.BatterySavingStats;
 import com.android.server.power.feature.PowerManagerFlags;
+//Ext add
+import android.os.HandlerThread;
 import com.android.server.wm.WindowManagerInternal;
 
 import dalvik.annotation.optimization.NeverCompile;
@@ -1261,6 +1263,10 @@ public final class PowerManagerService extends SystemService
     private SensorEventListener mProximityListener;
     private PowerManager.WakeLock mProximityWakeLock;
 
+    //Ext add
+    private final HandlerThread mBroadcastThread;
+    private final Handler mBroadcastHandler;
+
     public PowerManagerService(Context context) {
         this(context, new Injector());
     }
@@ -1382,6 +1388,11 @@ public final class PowerManagerService extends SystemService
             mNativeWrapper.nativeSetPowerMode(Mode.INTERACTIVE, true);
             mNativeWrapper.nativeSetPowerMode(Mode.DOUBLE_TAP_TO_WAKE, false);
             mInjector.invalidateIsInteractiveCaches();
+
+            //Ext add
+            mBroadcastThread = new HandlerThread("ScreenBroadcastThread");
+            mBroadcastThread.start(); 
+            mBroadcastHandler = new Handler(mBroadcastThread.getLooper());
         }
     }
 
@@ -3254,12 +3265,17 @@ public final class PowerManagerService extends SystemService
      * Ext add :
      * Send the broadcast when User timeout not to sleep
      */
-    private final Handler mBroadcastHandler = new Handler(FgThread.get().getLooper());
 
     private void sendTimeoutBroadcast() {
-        Intent extTimeoutIntent = new Intent("org.exthm.action.SCREEN_NEED_RELIGHT");
-        extTimeoutIntent.putExtra("state", 1);
-        mBroadcastHandler.post(() -> mContext.sendBroadcast(extTimeoutIntent));
+        mBroadcastHandler.post(() -> {
+            try {
+                Intent extTimeoutIntent = new Intent("org.exthm.action.SCREEN_NEED_RELIGHT");
+                extTimeoutIntent.putExtra("state", 1);
+                mContext.sendBroadcast(extTimeoutIntent);
+            } catch (Exception e) {
+                Slog.e(TAG, "Failed to send SCREEN_NEED_RELIGHT broadcast", e);
+            }
+        });
     }
 
     /**

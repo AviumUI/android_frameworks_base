20e249a684c786fb9ef0fa43e643ea7fca79a73d
ruyue <ruyueee@outlook.com>
2025-09-20 10:42:16 +0800

    feat(lockscreen): integrate and support custom lockscreen feature
    - Listen to custom lockscreen toggle state in KeyguardBlueprintInteractor and trigger system UI restart
    - Modify DefaultKeyguardBlueprint to dynamically replace clock and related views based on custom lockscreen toggle
    - Add custom lockscreen clock manager logic in NotificationPanelViewController
    - Monitor notification panel height changes and notify custom clock to update notification state
    
    Change-Id: Idee864ee61b8c7aaafbbd36d43d8b5f636542f0b

---
+++ packages/SystemUI/src/org/avium/systemui/lockscreen/CustomLockScreenClockFactory.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright (C) 2025-2026 The AviumUI Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.avium.systemui.lockscreen;
+
+import android.content.Context;
+import android.util.Log;
+import org.avium.systemui.lockscreen.type.bigboom.BigBoomClockController;
+import org.avium.systemui.lockscreen.util.CustomLockscreenSettings;
+import org.avium.systemui.lockscreen.type.smallcuteclock.SmallCuteClockController;
+
+public class CustomLockScreenClockFactory {
+
+    private static final String TAG = "AVIUM_LOCKSCREEN";
+
+    public static ICustomLockScreenClock create(Context context) {
+        if (!CustomLockscreenSettings.isEnabled()) {
+            return null;
+        }
+
+        int clockType = CustomLockscreenSettings.getClockType();
+        
+        switch (clockType) {
+            case 1:
+                return new BigBoomClockController();
+            case 2:
+                return new SmallCuteClockController();
+            default:
+                Log.w(TAG, "Unknown clock type: " + clockType);
+                return null;
+        }
+    }
+}

5991148075862bbb3034a1bdda9424bd031c5923
ruyue <ruyueee@outlook.com>
2025-09-22 19:48:17 +0800

    AviumLockScreen: support blur clock and add ios style clock
    Change-Id: I7a285fb03e297888e91c5084bfefb7c8e5de5bc4

--- packages/SystemUI/src/org/avium/systemui/lockscreen/type/guoguoclock/GuoguoClockController4.java
+++ packages/SystemUI/src/org/avium/systemui/lockscreen/type/guoguoclock/GuoguoClockController4.java
@@ -14,58 +14,63 @@
  * limitations under the License.
  */
 
-package org.avium.systemui.lockscreen.type.moremorethin;
+package org.avium.systemui.lockscreen.type.guoguoclock;
 
 import android.content.Context;
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.constraintlayout.widget.ConstraintSet;
 
+//import org.avium.test.R;
 import com.android.systemui.res.R;
+
 import org.avium.systemui.lockscreen.util.BaseLockscreenController;
-import org.avium.systemui.lockscreen.util.CustomLockscreenSettings;
 import org.avium.systemui.lockscreen.util.DigitalClockDisplayManager;
-import org.avium.systemui.lockscreen.util.GlassClockManager; 
+import org.avium.systemui.lockscreen.util.GlassClockManager;
 import org.avium.systemui.lockscreen.util.LockscreenClockUtils;
 import org.avium.systemui.lockscreen.util.LockscreenLayoutManager;
+import org.avium.systemui.lockscreen.util.CustomLockscreenSettings;
 
-public class MoreMoreThinClockController extends BaseLockscreenController {
+import java.util.Locale;
 
-    private static final int DIGIT_WIDTH_DP = 117;
-    private static final int DIGIT_HEIGHT_DP = 450;
+public class GuoguoClockController4 extends BaseLockscreenController {
 
+    private static final float SCALE_FACTOR = 0.5f;
+
+    private TextView mDateView;
     private ImageView mHour1, mHour2, mMinute1, mMinute2;
+    private ImageView mDotView;
     private ImageView[] mDigitViews;
     private DigitalClockDisplayManager mDigitalClockDisplayManager;
 
     private boolean mUseBlurEffect;
     private GlassClockManager mGlassClockManager;
-    
+
     private LockscreenLayoutManager mLayoutManager;
 
     private final int[] mDigitResources = new int[]{
-        R.drawable.moremorethin_0, R.drawable.moremorethin_1, R.drawable.moremorethin_2,
-        R.drawable.moremorethin_3, R.drawable.moremorethin_4, R.drawable.moremorethin_5,
-        R.drawable.moremorethin_6, R.drawable.moremorethin_7, R.drawable.moremorethin_8,
-        R.drawable.moremorethin_9
+            R.drawable.avium_guo_type4_0, R.drawable.avium_guo_type4_1, R.drawable.avium_guo_type4_2,
+            R.drawable.avium_guo_type4_3, R.drawable.avium_guo_type4_4, R.drawable.avium_guo_type4_5,
+            R.drawable.avium_guo_type4_6, R.drawable.avium_guo_type4_7, R.drawable.avium_guo_type4_8,
+            R.drawable.avium_guo_type4_9
     };
+    private final int mDotResource = R.drawable.avium_guo_type4_dot;
 
     @Override
     public View getView(Context context) {
         mContext = context;
-
-        mUseBlurEffect = "blur".equalsIgnoreCase(CustomLockscreenSettings.getClockColor().trim());
-
+        mUseBlurEffect =  "blur".equalsIgnoreCase(CustomLockscreenSettings.getClockColor().trim());
         createViews();
         mLayoutManager = new LockscreenLayoutManager(mContainer);
         setupLayout();
-    
         if (mUseBlurEffect) {
             mGlassClockManager.prepareWallpaper();
         }
         initializeCommonViews();
-        
         return mContainer;
     }
 
@@ -73,13 +78,33 @@ public class MoreMoreThinClockController extends BaseLockscreenController {
         mContainer = new ConstraintLayout(mContext);
         mContainer.setId(View.generateViewId());
 
+        mDateView = new TextView(mContext);
+        mDateView.setId(View.generateViewId());
+        mDateView.setTextColor(Color.WHITE);
+        mDateView.setTextSize(16f);
+        mDateView.getPaint().setShadowLayer(5, 0, 0, Color.BLACK);
+        mContainer.addView(mDateView);
+
+        Drawable sampleDigitDrawable = mContext.getResources().getDrawable(mDigitResources[0], mContext.getTheme());
+        int scaledDigitWidth = (int) (sampleDigitDrawable.getIntrinsicWidth() * SCALE_FACTOR);
+        int scaledDigitHeight = (int) (sampleDigitDrawable.getIntrinsicHeight() * SCALE_FACTOR);
+
+        Drawable dotDrawable = mContext.getResources().getDrawable(mDotResource, mContext.getTheme());
+        int scaledDotWidth = (int) (dotDrawable.getIntrinsicWidth() * SCALE_FACTOR);
+        int scaledDotHeight = (int) (dotDrawable.getIntrinsicHeight() * SCALE_FACTOR);
+
+        mDotView = createImageView();
+        mDotView.setLayoutParams(new ConstraintLayout.LayoutParams(scaledDotWidth, scaledDotHeight));
+        mDotView.setImageResource(mDotResource);
+        mContainer.addView(mDotView);
+
         if (mUseBlurEffect) {
             mGlassClockManager = new GlassClockManager(mContext, 4, mDigitResources);
             View[] digitViews = mGlassClockManager.getDigitViews();
             for (View iv : digitViews) {
                 iv.setId(View.generateViewId());
-                iv.setLayoutParams(new ConstraintLayout.LayoutParams(dpToPx(DIGIT_WIDTH_DP), dpToPx(DIGIT_HEIGHT_DP)));
-                iv.setAlpha(0.95f);
+                iv.setLayoutParams(new ConstraintLayout.LayoutParams(scaledDigitWidth, scaledDigitHeight));
+                iv.setAlpha(0.99f);
                 mContainer.addView(iv);
             }
         } else {
@@ -87,10 +112,9 @@ public class MoreMoreThinClockController extends BaseLockscreenController {
             mHour2 = createImageView();
             mMinute1 = createImageView();
             mMinute2 = createImageView();
-
             mDigitViews = new ImageView[]{mHour1, mHour2, mMinute1, mMinute2};
-            
             for (ImageView iv : mDigitViews) {
+                iv.setLayoutParams(new ConstraintLayout.LayoutParams(scaledDigitWidth, scaledDigitHeight));
                 mContainer.addView(iv);
             }
             mDigitalClockDisplayManager = new DigitalClockDisplayManager(mDigitViews, mDigitResources);
@@ -100,7 +124,6 @@ public class MoreMoreThinClockController extends BaseLockscreenController {
     private ImageView createImageView() {
         ImageView iv = new ImageView(mContext);
         iv.setId(View.generateViewId());
-        iv.setLayoutParams(new ConstraintLayout.LayoutParams(dpToPx(DIGIT_WIDTH_DP), dpToPx(DIGIT_HEIGHT_DP)));
         iv.setScaleType(ImageView.ScaleType.FIT_CENTER);
         return iv;
     }
@@ -114,30 +137,43 @@ public class MoreMoreThinClockController extends BaseLockscreenController {
             digitViews = mDigitViews;
         }
 
-        int[] clockViewIds = {digitViews[0].getId(), digitViews[1].getId(), digitViews[2].getId(), digitViews[3].getId()};
+        int dateId = mDateView.getId();
+        cs.connect(dateId, ConstraintSet.START, ConstraintSet.PARENT_ID, ConstraintSet.START);
+        cs.connect(dateId, ConstraintSet.END, ConstraintSet.PARENT_ID, ConstraintSet.END);
+
+        int[] clockChainIds = {
+                digitViews[0].getId(), digitViews[1].getId(),
+                mDotView.getId(),
+                digitViews[2].getId(), digitViews[3].getId()
+        };
 
         cs.createHorizontalChain(
-            ConstraintSet.PARENT_ID, ConstraintSet.LEFT,
-            ConstraintSet.PARENT_ID, ConstraintSet.RIGHT,
-            clockViewIds, null, ConstraintSet.CHAIN_PACKED
+                ConstraintSet.PARENT_ID, ConstraintSet.LEFT,
+                ConstraintSet.PARENT_ID, ConstraintSet.RIGHT,
+                clockChainIds, null, ConstraintSet.CHAIN_PACKED
         );
-        
-        int horizontalMarginPx = -dpToPx(20);
-        cs.setMargin(clockViewIds[1], ConstraintSet.START, horizontalMarginPx);
-        cs.setMargin(clockViewIds[2], ConstraintSet.START, horizontalMarginPx);
-        cs.setMargin(clockViewIds[3], ConstraintSet.START, horizontalMarginPx);
 
-        for (int id : clockViewIds) {
-            cs.connect(id, ConstraintSet.TOP, ConstraintSet.PARENT_ID, ConstraintSet.TOP);
-            cs.connect(id, ConstraintSet.BOTTOM, ConstraintSet.PARENT_ID, ConstraintSet.BOTTOM);
-            cs.setVerticalBias(id, 0.13f); 
+        cs.connect(clockChainIds[0], ConstraintSet.TOP, dateId, ConstraintSet.BOTTOM, dpToPx(12));
+
+        for (int id : clockChainIds) {
+            cs.connect(id, ConstraintSet.TOP, clockChainIds[0], ConstraintSet.TOP);
+            cs.connect(id, ConstraintSet.BOTTOM, clockChainIds[0], ConstraintSet.BOTTOM);
         }
+
+        int[] verticalChainIds = {dateId, clockChainIds[0]};
+        cs.createVerticalChain(
+                ConstraintSet.PARENT_ID, ConstraintSet.TOP,
+                ConstraintSet.PARENT_ID, ConstraintSet.BOTTOM,
+                verticalChainIds, null, ConstraintSet.CHAIN_PACKED
+        );
+        cs.setVerticalBias(dateId, 0.18f);
 
         mLayoutManager.applyLayoutChanges();
     }
 
     @Override
     public void onTimeTick() {
+        mDateView.setText(LockscreenClockUtils.getCurrentTimeString("M月d日 EEEE", Locale.CHINESE));
         String timeString = LockscreenClockUtils.getCurrentTimeString("HHmm");
         if (mUseBlurEffect) {
             mGlassClockManager.updateTime(timeString);
@@ -147,7 +183,8 @@ public class MoreMoreThinClockController extends BaseLockscreenController {
     }
 
     @Override
-    public void onNotificationStateChanged(boolean hasNotifications) {}
+    public void onNotificationStateChanged(boolean hasNotifications) {
+    }
 
     @Override
     public void applyStyles() {
@@ -165,6 +202,7 @@ public class MoreMoreThinClockController extends BaseLockscreenController {
         }
         mContext = null;
         mContainer = null;
+        mDateView = null;
         mDigitalClockDisplayManager = null;
     }
 

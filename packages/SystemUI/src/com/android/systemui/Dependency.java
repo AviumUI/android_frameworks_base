24f05888f0142fc461ca4505ed5b7d82ba968ba5
ruyue <ruyueee@outlook.com>
2025-10-12 20:19:29 +0800

    fixup: drop legacy NotificationMediaManager import
    Change-Id: I7d9acbafb08c85b08fbde9ffbff2ed83d2bae2a1

--- packages/SystemUI/src/com/android/systemui/Dependency.java
+++ packages/SystemUI/src/com/android/systemui/Dependency.java
@@ -44,7 +44,6 @@ import com.android.systemui.plugins.statusbar.StatusBarStateController;
 import com.android.systemui.settings.UserTracker;
 import com.android.systemui.statusbar.CommandQueue;
 import com.android.systemui.statusbar.NotificationListener;
-import com.android.systemui.statusbar.NotificationMediaManager;
 import com.android.systemui.statusbar.notification.collection.render.GroupExpansionManager;
 import com.android.systemui.statusbar.notification.collection.render.GroupMembershipManager;
 import com.android.systemui.statusbar.notification.stack.AmbientState;

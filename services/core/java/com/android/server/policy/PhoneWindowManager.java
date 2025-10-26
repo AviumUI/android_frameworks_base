96c15880abcac3bf1c6fa5b98c43c800066dffcd
ruyue <ruyueee@outlook.com>
2025-10-26 14:14:52 +0800

    PWM: Allow enable popupview gesture
    Change-Id: I7a4077bb2df18caa2d07f2c54d68cfdc7024b355

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -232,6 +232,7 @@ import android.graphics.Point;
 import android.content.ComponentName;
 import android.app.PendingIntent;
 import android.window.DesktopExperienceFlags;
+import android.os.SystemProperties;
 
 import com.android.internal.R;
 import com.android.internal.annotations.VisibleForTesting;
@@ -6699,6 +6700,10 @@ public class PhoneWindowManager implements WindowManagerPolicy {
 
     @Override
     public int interceptMotionBeforeQueueing(MotionEvent event) {
+        boolean isPopupViewEnable = SystemProperties.getBoolean("persist.avium.popup_gesture", true);
+        if(!isPopupViewEnable){
+            return SYSTEM_GESTURE_NONE;
+        }
         final int action = event.getActionMasked();
         final float x = event.getRawX();
         final float y = event.getRawY();

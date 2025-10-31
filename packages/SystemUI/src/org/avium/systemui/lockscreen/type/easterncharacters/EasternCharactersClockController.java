539f3734e98e4ca054985935712976958c80de28
ruyue <ruyueee@outlook.com>
2025-10-31 23:04:31 +0800

    AviumLockscreen: Overall optimization
    Change-Id: Ib565c65dc64b74e9c1107e9aa0fc81f89e50e851

--- packages/SystemUI/src/org/avium/systemui/lockscreen/type/easterncharacters/EasternCharactersClockController.java
+++ packages/SystemUI/src/org/avium/systemui/lockscreen/type/easterncharacters/EasternCharactersClockController.java
@@ -32,6 +32,7 @@ import androidx.constraintlayout.widget.ConstraintSet;
 import androidx.core.content.res.ResourcesCompat;
 
 import com.android.systemui.res.R;
+//import org.avium.aviumlockscreenstudio.R;
 
 import org.avium.systemui.lockscreen.util.BaseLockscreenController;
 import org.avium.systemui.lockscreen.util.CustomLockscreenSettings;
@@ -166,13 +167,12 @@ public class EasternCharactersClockController extends BaseLockscreenController {
 
     private void updateDateInfo() {
         Date now = new Date();
-        SimpleDateFormat weekDayFormat = new SimpleDateFormat("EEEEa", Locale.CHINESE);
-        SimpleDateFormat dateFormat = new SimpleDateFormat("M月d日 H:mm", Locale.CHINESE);
+        String dateFormat = mContext.getString(R.string.date_format);
+        SimpleDateFormat weekDayFormat = new SimpleDateFormat(dateFormat, Locale.getDefault());
         
-        String weekDayStr = weekDayFormat.format(now).replace("星期", "周");
-        String dateStr = dateFormat.format(now);
+        String dateStr = weekDayFormat.format(now);
         
-        mDateInfoView.setText(String.format("%s\n%s", weekDayStr, dateStr));
+        mDateInfoView.setText(String.format("%s", dateStr));
     }
     
     private void updateTextView(TextView view, String text) {

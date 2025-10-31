3debf924447959f597ba67540cfb378dc20940c6
ruyue <ruyueee@outlook.com>
2025-10-31 23:04:31 +0800

    AviumLockscreen: Overall optimization
    Change-Id: Ib565c65dc64b74e9c1107e9aa0fc81f89e50e851

--- packages/SystemUI/src/org/avium/systemui/lockscreen/util/GlassClockManager.java
+++ packages/SystemUI/src/org/avium/systemui/lockscreen/util/GlassClockManager.java
@@ -29,6 +29,8 @@ public class GlassClockManager {
     private Bitmap mBlurredWallpaperBitmap;
     private final DigitView[] mDigitViews;
     private final int[] mDigitResources;
+    private DigitView mDotView;
+    private int mDotResource;
 
     public GlassClockManager(Context context, int numDigits, int[] digitResources) {
         this.mContext = context;
@@ -40,8 +42,18 @@ public class GlassClockManager {
         }
     }
 
+    public void setDotResource(int dotResource) {
+        this.mDotResource = dotResource;
+        this.mDotView = new DigitView(mContext);
+        this.mDotView.setDigitDrawable(ContextCompat.getDrawable(mContext, mDotResource));
+    }
+
     public View[] getDigitViews() {
         return mDigitViews;
+    }
+
+    public View getDotView() {
+        return mDotView;
     }
 
     public void prepareWallpaper() {
@@ -70,6 +82,10 @@ public class GlassClockManager {
 
         for (DigitView digitView : mDigitViews) {
             digitView.invalidate();
+        }
+        
+        if (mDotView != null) {
+            mDotView.invalidate();
         }
     }
 

c024fa63291bbc6bc3a600ac4be3c89b2cd73669
ruyue <ruyueee@outlook.com>
2025-11-06 16:26:45 +0800

    PWM: Support horizontal screen gestures
    Change-Id: I0442984f33a1713d4e358625d1a95c592fd211fe

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -6793,6 +6793,38 @@ public class PhoneWindowManager implements WindowManagerPolicy {
         }
     }
 
+    private final DisplayManager.DisplayListener mDisplayListener =
+        new DisplayManager.DisplayListener() {
+        @Override
+        public void onDisplayAdded(int displayId) {}
+
+        @Override
+        public void onDisplayRemoved(int displayId) {}
+
+        @Override
+        public void onDisplayChanged(int displayId) {
+            if (displayId == Display.DEFAULT_DISPLAY) {
+                updateGestureParams();
+                mIsTrackingSideGesture = false;
+                mGestureTriggered = false;
+            }
+        }
+    };
+
+    private void updateGestureParams() {
+        Display display = mDisplayManager.getDisplay(Display.DEFAULT_DISPLAY);
+        if (display != null) {
+            Point size = new Point();
+            display.getRealSize(size);
+            mDisplayWidth = size.x;
+            mDisplayHeight = size.y;
+        }
+        float density = mContext.getResources().getDisplayMetrics().density;
+        mMinGestureDistancePx = TRIGGER_MIN_DISTANCE_DP * density;
+        mGestureAreaHeightPx  = GESTURE_AREA_HEIGHT_DP  * density;
+        mGestureAreaWidthPx   = GESTURE_AREA_WIDTH_DP   * density;
+    }
+
 
 
     /** {@inheritDoc} */
@@ -6802,19 +6834,8 @@ public class PhoneWindowManager implements WindowManagerPolicy {
         // So it is better not to bind keyguard here.
         mKeyguardDelegate.onSystemReady();
         //Ext add
-        Display display = mDisplayManager.getDisplay(Display.DEFAULT_DISPLAY);
-        Point displaySize = new Point();
-        display.getRealSize(displaySize);
-        mDisplayWidth = displaySize.x;
-        mDisplayHeight = displaySize.y;
-        float density = mContext.getResources().getDisplayMetrics().density;
-        
-        mMinGestureDistancePx = TRIGGER_MIN_DISTANCE_DP * density;
-        mGestureAreaHeightPx = GESTURE_AREA_HEIGHT_DP * density;
-        mGestureAreaWidthPx = GESTURE_AREA_WIDTH_DP * density;
-        
-        Slog.i(TAG_GESTURE, "Gesture parameters initialized: minDistance=" + mMinGestureDistancePx +
-                "px, areaHeight=" + mGestureAreaHeightPx + "px, areaWidth=" + mGestureAreaWidthPx + "px");
+        updateGestureParams();
+        mDisplayManager.registerDisplayListener(mDisplayListener, mHandler);
 
         mModifierShortcutManager.onSystemReady();
 

7a02f753eb40b4f32f8850fb03777e24f0803165
ruyue <ruyueee@outlook.com>
2025-11-20 18:40:10 +0800

    feat(Lock Screen): Add support for DepthWallpaper feature
    Change-Id: I07c08c39f7bf8937e112280cdf2f64ee3d471823

--- packages/SystemUI/src/org/avium/systemui/lockscreen/type/normaltime/NormalTimeClockController.java
+++ packages/SystemUI/src/org/avium/systemui/lockscreen/type/normaltime/NormalTimeClockController.java
@@ -35,6 +35,9 @@ import org.avium.systemui.lockscreen.util.DigitalClockDisplayManager;
 import org.avium.systemui.lockscreen.util.GlassClockManager;
 import org.avium.systemui.lockscreen.util.LockscreenClockUtils;
 import org.avium.systemui.lockscreen.util.LockscreenLayoutManager;
+import org.avium.systemui.depthwallpaper.DepthWallpaperAttacher;
+import org.avium.systemui.depthwallpaper.DepthWallpaperSetup;
+
 
 import java.util.Locale;
 
@@ -72,7 +75,9 @@ public class NormalTimeClockController extends BaseLockscreenController {
         }
 
         initializeCommonViews();
-        return mContainer;
+        DepthWallpaperSetup.INSTANCE.applyIfNeeded(context);
+        View wrapped = DepthWallpaperAttacher.INSTANCE.wrapIfNeeded(mContainer);
+        return wrapped;
     }
 
     private void createViews() {
--- packages/SystemUI/src/org/avium/systemui/lockscreen/type/thinlongclock/ThinLongClockController.java
+++ packages/SystemUI/src/org/avium/systemui/lockscreen/type/thinlongclock/ThinLongClockController.java
@@ -33,6 +33,9 @@ import org.avium.systemui.lockscreen.util.DigitalClockDisplayManager;
 import org.avium.systemui.lockscreen.util.GlassClockManager;
 import org.avium.systemui.lockscreen.util.LockscreenClockUtils;
 import org.avium.systemui.lockscreen.util.LockscreenLayoutManager;
+import org.avium.systemui.depthwallpaper.DepthWallpaperAttacher;
+import org.avium.systemui.depthwallpaper.DepthWallpaperSetup;
+
 
 import java.util.Locale;
 
@@ -74,7 +77,9 @@ public class ThinLongClockController extends BaseLockscreenController {
         }
 
         initializeCommonViews();
-        return mContainer;
+        DepthWallpaperSetup.INSTANCE.applyIfNeeded(context);
+        View wrapped = DepthWallpaperAttacher.INSTANCE.wrapIfNeeded(mContainer);
+        return wrapped;
     }
 
     private void createViews() {

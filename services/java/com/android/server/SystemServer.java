817a2396894ca160d4269b5205c68043d9c1c2bf
rmp22 <195054967+rmp22@users.noreply.github.com>
2025-12-25 06:09:41 +0800

    base: Add keybox from AxionAOSP
    Tricky store port integrate with additional fixes to octet,
    key handling and certificate hacks
    
    Change-Id: Id5b1161c827abe141b7b58976c8e97083ff9967f
    Co-authored-by: Dakkshesh <beakthoven@gmail.com>
    Co-authored-by: 5ec1cff <ewtqyqyewtqyqy@gmail.com>
    Signed-off-by: rmp22 <195054967+rmp22@users.noreply.github.com>

--- services/java/com/android/server/SystemServer.java
+++ services/java/com/android/server/SystemServer.java
@@ -1772,6 +1772,17 @@ public final class SystemServer implements Dumpable {
             mSystemServiceManager.startService(FreeformOverlayManagerService.class);
             t.traceEnd();
 
+            // latency test ONLY
+            // this is probably no-op, intializes the tricky store instance for system server 
+            // for system use cases - nte: maybe not needed at all, since keystore/cert requests are per-app context
+            t.traceBegin("StartTrickyStoreService");
+            try {
+                android.security.trickystore.TrickyStoreService.getInstance().initialize();
+            } catch (Throwable e) {
+                Slog.e(TAG, "Failed to initialize TrickyStoreService", e);
+            }
+            t.traceEnd();
+
             if (mFactoryTestMode == FactoryTest.FACTORY_TEST_LOW_LEVEL) {
                 Slog.i(TAG, "No Bluetooth Service (factory test)");
             } else if (!context.getPackageManager().hasSystemFeature

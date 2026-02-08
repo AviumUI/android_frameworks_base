dbf0600cdd7f7588ac5a0f840fa2144b3256e41d
ruyue <ruyueee@outlook.com>
2026-02-08 17:40:20 +0800

    NMS: Fix the storage method of the package list
    Change-Id: I6a6ad49d4765c6d0e5897833462bfb80d9dd21ee

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -13877,6 +13877,14 @@ public final class Settings {
          */
         @Readable
         public static final String STATUS_BAR_SHOW_LYRIC = "status_bar_show_lyric";
+
+        /** 
+         * 
+         * @hide 
+         * 
+         */
+        @Readable
+        public static final String AVIUM_DISABLE_SENSOR_APPS = "avium_disable_sensor_apps";
         
         /**
          * String property which contains the package name of the contextual
@@ -21528,6 +21536,14 @@ public final class Settings {
             public static final String WEAR_SYSTEM_STATUS_TRAY_CONFIGURATION =
                     "wear_system_status_tray_configuration";
         }
+
+        /** 
+         * Ext add
+         * @hide 
+         * 
+         */
+        @Readable
+        public static final String AVIUM_HIDE_SYSAPP_NOTIFS = "avium_hide_sysapp_notifs";
     }
 
     /**

e5a9bb8e7aa5c093f96ab766f3a528f90629284c
Hecheng Yu <kde-yyds@qq.com>
2026-02-15 12:08:27 +0800

    Introduce Modded Pop-Up View
    Squashed commit:
    1. base: Add Pop-Up View support
    	Arman-ATI: Adapt for 16-qpr0
    	kde-yyds: Adapt for 16-qpr2
    2. Add PopUP dependencies from Sun-sdk
    	Arman-ATI: Adapted classes
    	kde-yyds: Fixup runtime crashes
    3. Avoid InputChannel of Gesture Monitor being Disposed
    	Due to 16-qpr2 changes, gesture monitor's InputChannel is disposed soon after initialization due to Pop-Up View changes, which breaks system gestures. As a workaround, this commit adds a setServerRetained() function to keep the gesture monitor.
    4. Partially Refactor Pop-Up View
    	In RisingOS Revived, the ported Pop-Up View is quite unstable. When dragging mini edge bar or pinned window overlay, there's race condition that randomly crashes system_server. Rather than finding the real cause of it, refactoring the mini window logic to avoid touching the unstable code is more simple.
    	The original Pop-Up View implementation monitors the InputChannel itself, and relies on TaskPositioningController API to position window. Both behaviors have been deprecated since Android 16, so we should no longer use them.
    	Without public InputChannel and TaskPositioningController, dragging on edge bar and pinned window overlay is broken. So we need to refactor the logic to use more standard move/resize methods.
    	This commit introduced a fresh re-design of Pop-Up View:
    	Overview
    		A white pill-shaped bar is above the freeform window. Dragging around the bar can move the freeform window. Clicking the bar will show a menu for maximize/minimize. Dragging one of the 4 corners can resize (actually scale) the window.
    	Changes
    		DimmerWindow: It's no longer a dimmer window. Instead, it's an overlay with top white bar and 4 invisble resize handles. A small popup menu with window maximize and minimize controls is also implemented.
    		DiimerWindow: Touch listeners are added for tracking window focus and for performing window move/resize.
    		PopUpWindowController: We are using a short click effect for vibration, instead of traditional long wave effect.
    		PopUpWindowController: Hide custom window control menu when touch events arrive.
    		BackNavigationController: Send back navigation to fullscreen window below Pop-Up View when focus should be there.
    		BLASTSyncEngine: Skip sync when Pop-Up View window is visible but not part of sync group, to avoid unwanted sync timeout.
    5. Clean Up Unused Code for Pinned Pop-Up Window
    	With our current re-design, we only use Mini Window Mode (102). Pinned Window Mode (101) is deprecated. This commit removes all code and resources for pinned window.
    Change-Id: I2c24ab7a9389b7bbb64b59eb7f1f123c2361312c
    
    Change-Id: I68432b80ed3f7b762bd347fd12fc4f955fed710d

--- services/core/java/com/android/server/wm/ActivityRecord.java
+++ services/core/java/com/android/server/wm/ActivityRecord.java
@@ -206,6 +206,9 @@ import static com.android.server.wm.ActivityTaskManagerService.RELAUNCH_REASON_N
 import static com.android.server.wm.ActivityTaskManagerService.RELAUNCH_REASON_WINDOWING_MODE_RESIZE;
 import static com.android.server.wm.ActivityTaskManagerService.getInputDispatchingTimeoutMillisLocked;
 import static com.android.server.wm.ActivityTaskManagerService.isPip2ExperimentEnabled;
+import static com.android.server.wm.PopUpAnimationController.ANIMATION_CROSS_OVER_EXIT_DURATION;
+import static com.android.server.wm.PopUpWindowController.MOVE_TO_BACK_NON_USER;
+import static com.android.server.wm.PopUpWindowController.PACKAGE_NAME_SYSTEM_TOOL;
 import static com.android.server.wm.StartingData.AFTER_TRANSACTION_COPY_TO_CLIENT;
 import static com.android.server.wm.StartingData.AFTER_TRANSACTION_IDLE;
 import static com.android.server.wm.StartingData.AFTER_TRANSACTION_REMOVE_DIRECTLY;
@@ -441,6 +444,8 @@ final class ActivityRecord extends WindowToken {
     final ComponentName mActivityComponent;
     // Input application handle used by the input dispatcher.
     private InputApplicationHandle mInputApplicationHandle;
+
+    final boolean isSystemTool;
 
     final int launchedFromPid; // always the pid who started the activity.
     final int launchedFromUid; // always the uid who started the activity.
@@ -1858,6 +1863,7 @@ final class ActivityRecord extends WindowToken {
         info = aInfo;
         mUserId = UserHandle.getUserId(info.applicationInfo.uid);
         packageName = info.applicationInfo.packageName;
+        isSystemTool = PACKAGE_NAME_SYSTEM_TOOL.equals(packageName);
         intent = _intent;
 
         // If the class name in the intent doesn't match that of the target, this is probably an
@@ -2411,6 +2417,10 @@ final class ActivityRecord extends WindowToken {
     private int getStartingWindowType(boolean newTask, boolean taskSwitch, boolean processRunning,
             boolean allowTaskSnapshot, boolean activityCreated, boolean activityAllDrawn,
             TaskSnapshot snapshot) {
+        if (isSystemTool) {
+            return processRunning ? STARTING_WINDOW_TYPE_NONE : STARTING_WINDOW_TYPE_SNAPSHOT;
+        }
+
         // A special case that a new activity is launching to an existing task which is moving to
         // front. If the launching activity is the one that started the task, it could be a
         // trampoline that will be always created and finished immediately. Then give a chance to
@@ -3119,6 +3129,15 @@ final class ActivityRecord extends WindowToken {
         return super.isFocusable() && (canReceiveKeys() || isAlwaysFocusable());
     }
 
+    boolean isFocusableOrPopUpView() {
+        return super.isFocusable() && (canReceiveKeys() || isAlwaysFocusable() || isPopUpView());
+    }
+
+    boolean isPopUpView() {
+        return getWindowConfiguration().isPopUpWindowMode() ||
+                (getRootTask() != null && getRootTask().getWindowConfiguration().isPopUpWindowMode());
+    }
+
     boolean canReceiveKeys() {
         return getWindowConfiguration().canReceiveKeys() && !mWaitForEnteringPinnedMode;
     }
@@ -3192,6 +3211,9 @@ final class ActivityRecord extends WindowToken {
 
     /** @return whether this activity is non-resizeable but is forced to be resizable. */
     boolean canForceResizeNonResizable(int windowingMode) {
+        if (getWindowConfiguration().isPopUpWindowMode()) {
+            return false;
+        }
         if (windowingMode == WINDOWING_MODE_PINNED && info.supportsPictureInPicture()) {
             return false;
         }
@@ -3648,7 +3670,7 @@ final class ActivityRecord extends WindowToken {
             Transition newTransition = null;
             if (!chain.isCollecting()) {
                 chain.attachTransition(
-                        mTransitionController.requestCloseTransitionIfNeeded(trigger));
+                        mTransitionController.requestCloseTransitionIfNeeded(trigger, endTask || getChildCount() == 0));
                 newTransition = chain.getTransition();
             }
             if (chain.isCollecting()) {
@@ -3665,6 +3687,7 @@ final class ActivityRecord extends WindowToken {
             // the next focusable task should be focused.
             if (mayAdjustTop && task.topRunningActivity(true /* focusableOnly */)
                     == null) {
+                task.mWindowContainerExt.setFinishTopTask(true);
                 task.adjustFocusToNextFocusableTask("finish-top", false /* allowFocusSelf */,
                         shouldAdjustGlobalFocus);
             }
@@ -4331,9 +4354,6 @@ final class ActivityRecord extends WindowToken {
         final WindowContainer trigger = remove && task != null && task.getChildCount() == 1
                 ? task : this;
         final ActionChain chain = mAtmService.mChainTracker.startTransit("appDied");
-        if (!chain.isCollecting()) {
-            chain.attachTransition(mTransitionController.requestCloseTransitionIfNeeded(trigger));
-        }
         chain.collectClose(trigger);
         cleanUp(true /* cleanServices */, true /* setState */);
         if (remove) {
@@ -6110,7 +6130,7 @@ final class ActivityRecord extends WindowToken {
      */
     @VisibleForTesting
     boolean shouldPauseActivity(ActivityRecord activeActivity) {
-        return shouldMakeActive(activeActivity) && !isFocusable() && !isState(PAUSING, PAUSED)
+        return shouldMakeActive(activeActivity) && !isFocusableOrPopUpView() && !isState(PAUSING, PAUSED)
                 // We will only allow pausing if results is null, otherwise it will cause this
                 // activity to resume before getting result
                 && (results == null);
@@ -6133,7 +6153,7 @@ final class ActivityRecord extends WindowToken {
      * - should be focusable
      */
     private boolean shouldBeResumed(ActivityRecord activeActivity) {
-        return shouldMakeActive(activeActivity) && isFocusable()
+        return shouldMakeActive(activeActivity) && isFocusableOrPopUpView()
                 && getTaskFragment().getVisibility(activeActivity)
                         == TASK_FRAGMENT_VISIBILITY_VISIBLE
                 && canResumeByCompat();
@@ -8204,6 +8224,11 @@ final class ActivityRecord extends WindowToken {
         computeConfigByResolveHint(getResolvedOverrideConfiguration(), newParentConfig);
         aspectRatioPolicy.setLetterboxBoundsForFixedOrientationAndAspectRatio(
                 new Rect(resolvedBounds));
+
+	if (newParentConfig.windowConfiguration.isPopUpWindowMode() || isPopUpView()) {
+            getResolvedOverrideConfiguration().unset();
+            return;
+        }
     }
 
     @Override
@@ -8272,6 +8297,7 @@ final class ActivityRecord extends WindowToken {
                     // as a part of WindowOrganizerController#finishTransition().
                     // If not checked the activity might be collected for the wrong transition,
                     // such as a TRANSIT_OPEN transition requested right after TRANSIT_PIP.
+                    && !isPopUpView()
                     && !(mWaitForEnteringPinnedMode
                     && mTransitionController.inFinishingTransition(this))) {
                 mTransitionController.collect(this);
@@ -8511,6 +8537,8 @@ final class ActivityRecord extends WindowToken {
             Slog.wtf(TAG, "trying to update reported(client) config while dispatch is paused");
         }
         ProtoLog.v(WM_DEBUG_CONFIGURATION, "Ensuring correct configuration: %s", this);
+
+        PopUpWindowController.getInstance().ensureActivityConfiguration(this);
 
         final int newDisplayId = getDisplayId();
         final boolean displayChanged = mLastReportedDisplayId != newDisplayId;

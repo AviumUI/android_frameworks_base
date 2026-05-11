8a3e89e6cbff2c3e869904989854729a419efa17
ruyue <ruyueee@outlook.com>
2026-05-11 16:04:01 +0800

    SystemUI: Disable view the keyguard scrimView
    It dims the wallpaper, which many users find unpleasant and ruins the visual appeal. Therefore, we can provide users with customizable options :3
    
    Change-Id: I598f3059dea2abd94702ca289f4afd3dbf41fff8

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/ScrimController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/ScrimController.java
@@ -104,6 +104,11 @@ import java.util.function.Consumer;
 
 import javax.inject.Inject;
 
+/*
+ * Ext add
+ */
+import android.os.SystemProperties;
+
 /**
  * Controls both the scrim behind the notifications and in front of the notifications (when a
  * security method gets shown). Unused when the scene_container flag is enabled.
@@ -1043,6 +1048,11 @@ public class ScrimController implements ViewTreeObserver.OnPreDrawListener, Dump
         mInFrontAlpha = mState.getFrontAlpha();
         mBehindAlpha = mState.getBehindAlpha();
         mNotificationsAlpha = mState.getNotifAlpha();
+        /*
+         * Ext add
+         * disable to view scrim
+         */
+        boolean isViewScrim = SystemProperties.getBoolean("persist.avium.lockscreendim", false);
 
         assertAlphasValid();
 
@@ -1109,6 +1119,16 @@ public class ScrimController implements ViewTreeObserver.OnPreDrawListener, Dump
             Pair<Integer, Float> result = calculateBackStateForState(mState);
             int behindTint = result.first;
             float behindAlpha = result.second;
+
+            /*
+             * Ext add
+             * Remove the scrimView on lock screen
+             */
+            if (mState == ScrimState.KEYGUARD && isViewScrim) {
+                behindAlpha = 0.0f;
+                behindTint = Color.TRANSPARENT;
+            }
+
             if (mTransitionToFullShadeProgress > 0.0f) {
                 Pair<Integer, Float> shadeResult = calculateBackStateForState(
                         ScrimState.SHADE_LOCKED);
